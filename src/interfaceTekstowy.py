1b337a18b30878efafed94c26c90120b1025d089
BuildTools <fenderpass@gmail.com>
2020-06-22 11:46:02 +0200

    Ukonczenie klasy Plik w obslugaDanych.py i przeniesienie teogo pliku z folderu do folderu nadrzedengo

--- src/interfaceTekstowy.py
+++ src/interfaceTekstowy.py
@@ -1,5 +1,6 @@
 from src.komunikaty import menuGlownePL as menuGlowne
 from src.komunikaty import interfaceTekstowyPL as interfaceTekstowy
+import src.obslugaDanych;
 def wczytanieInta():
     ok = True
     while(True):
@@ -17,6 +18,28 @@ def wczytanieInta():
 class interface:
     def __init__(self):
         self.menuGlowne()
+        self.db={}
+        self.wczytajDane()
+        self.plik= src.Plik("data.txt")
     def menuGlowne(self):
         print(menuGlowne.menuGlowneKomunikat)
+    def wczytajDane(self):
+        try:
+            self.db=self.plik.wczytajJson()
+        except Exception as ex:
+            print(ex)
+            exit(2139)
 
+'''
+version: 1.0
+listy_zakupów:
+    Lista sporzywacza:
+        kto co kupił gdziek keidy za ile
+    Lista paliwowa:
+        ----||----
+settings:
+    wartość_defaultowa_kto_kupuje: 
+    wartość_defaultowa_kiedy_kupuje: 
+    ostatnio_edytowana_lista: 
+    szerokosc_podstawowa: 
+'''
--- src/obslugaDanych/obslugaDanych.py
+++
@@ -1,6 +0,0 @@
-import src.Exceptions as Exceptions
-class Plik:
-    def __int__(self,nazwaPliku):
-        if(len(nazwaPliku)==0):
-            raise Exceptions.zaKrotkiString();
-        self.nazwaPliku=nazwaPliku
